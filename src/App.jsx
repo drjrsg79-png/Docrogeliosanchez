9857b193b853b2e1af3473d978b63f92b6993f47
drjrsg79-png <drjrsg79@gmail.com>
2026-08-15 02:55:06 -0600

    Add routing with Home component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
 
+function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Dr. Rogelio Sánchez</h1>
+      <p>Orientación y seguimiento para pacientes</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
